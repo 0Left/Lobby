6927bb7b2df68bdf0f3fb95bc321e74a68f147e5
0Left <leftamail@gmail.com>
2025-07-07 23:52:45 -0300

    update

--- src/UserInfoPrompt.jsx
+++ src/UserInfoPrompt.jsx
@@ -24,6 +24,7 @@ export default function UserInfoPrompt({ onSubmit, initialGameId }) {
       onSubmit({ name, userId, game: selectedGame });
     }
   };
+  
 
   const game = gameCategories.find(g => g.id === selectedGame);
 
